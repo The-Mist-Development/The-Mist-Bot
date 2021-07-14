193c691677e8f8142395ae0fa2c3e95660bbbeb6
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2021-07-14 20:17:05 +0100

    potential working number extraction

--- index.js
+++ index.js
@@ -360,14 +360,15 @@ function doCounting(message) {
 }
 
 function continueCounting(message, row) {
-  let count = row.toString().slice(24);
+  let countString = row.toString().slice(24);
+  let counte = parseInt(countString);
   let userInput = parseInt(message.content, 10);
-  message.channel.send("count " + count);
+  message.channel.send("count " + counte);
   message.channel.send("input " + userInput);
   return;
-  if (userInput === count + 1) {
+  if (userInput === counte + 1) {
     message.react("☑");
-    dbClient.query("UPDATE exclusive SET value = " + (count + 1).toString() + "WHERE key='count'");
+    dbClient.query("UPDATE exclusive SET value = " + (counte + 1).toString() + "WHERE key='count'");
   }
   else {
     dbClient.query("UPDATE exclusive SET value = 1 WHERE key='count'");
