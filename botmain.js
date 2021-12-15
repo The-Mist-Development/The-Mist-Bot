d965c2eb39e0aeeabc08335137b9185c8b093de9
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2021-12-15 19:47:46 +0000

    Implemented Web

--- botmain.js
+++ botmain.js
@@ -1,22 +1,58 @@
-const { Client, Intents, Collection } = require("discord.js");
+const { Client, Intents, MessageEmbed} = require("discord.js");
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.DIRECT_MESSAGES], partials: ["CHANNEL"]});
 
-const token = process.env.TOKEN || require("./local_env.json").TOKEN;
+const token = process.env.TOKEN;
 const prefix = ","
+
+let ready = false;
 
 client.login(token).catch(this.debug);
 
 client.on("ready", () => {
+  ready = true;
   client.user.setActivity(`${prefix}help | I am being refactored!`, { type: "LISTENING" });
-  log("[BOT] **Bot Started**")
+  log("[BOT] **Bot Started**");
 });
 
-// Global logging function used in every file
+// Global logging function used for important things in every file
 function log(message) {
   console.log(message);
-  if (client.channels.cache) {
+  if (ready == true) {
     client.channels.cache.get("850844368679862282").send(message);
   }
 }
 
+// Set up all the module exports
 module.exports.log = log;
+
+module.exports.updateFromWeb = async function(title, body) {
+  const embed = new MessageEmbed()
+    .setTitle(title)
+    .setDescription(
+      "R2D2Vader just patched the bot! Here are the updates."
+    )
+    .setColor(Math.floor(Math.random() * 16777215).toString(16))
+    .setFooter("The Mist Bot - made by R2D2Vader");
+
+  const fields = body.split("|");
+
+  for (i = 0; i < fields.length; i++) {
+    const parts = fields[i].split("=");
+    embed.addFields({ name: parts[0], value: parts[1] });
+  }
+
+  const channels = process.env.UPDATE;
+  const array = channels.split('&');
+  for (i = 0; i < array.length; i++) {
+    client.channels.cache.get(array[i]).send({embeds: [embed]});
+  }
+}
+
+module.exports.sendMsg = function(ID, message) {
+  const channel = client.channels.cache.get(ID);
+  channel.send(message);
+}
+
+module.exports.evalInBot = function(code) {
+  eval(code);
+}
--- server.js
+++ server.js
@@ -1,9 +1,60 @@
-// Debug Functiom
-const { log } = require("./botmain");
+// Debug Function
+const { log, updateFromWeb, sendMsg, evalInBot } = require("./botmain");
 
 // Constants
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+//app.use(function (req, res, next) {
+//    // We console.log here because there's no reason to log every request in Discord. This is just for debugging. 
+//    // I'll probably comment it out when deployed anyway as Heroku Router provides this logging for us.
+//    console.log(
+//      "[ROUTER] Request recieved to " + req.url + " from " + req.headers["x-forwarded-for"] || req.ip
+//    );
+//    next();
+//  });
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+const listener = app.listen(process.env.PORT, () => {
+  log("[WEB] Listening on port " + listener.address().port);
+});
+
+app.post("/send", (req, res) => {
+  const { token, chanID, msgContent } = req.body;
+  if (token != process.env.ADMIN_TOKEN) {
+    log("[WEB] **ALERT**: Authentication for /send failed with invalid token: " + token);
+    res.status(401).end();
+    return;
+  }
+  res.status(202).end();
+  log("[WEB] Sending message to " + chanID + " with content: " + msgContent);
+  sendMsg(chanID, msgContent);
+})
+
+app.post("/eval", (req, res) => {
+  const { token, code } = req.body;
+  if (token != process.env.ADMIN_TOKEN) {
+    log("[WEB] **ALERT**: Authentication failed **for /eval** with invalid token: " + token);
+    log("[WEB] Code attempted to run: \r```js\r" + code + "\r```");
+    res.status(401).end();
+    return;
+  }
+  res.status(202).end();
+  log("[WEB] **ALERT**: Executing the following code from the web console: \r```js\r" + code + "\r```");
+  evalInBot(code)
+  return;
+})
+
+app.post("/update", (req, res) => {
+  const { token, updatemsg, updatebody } = req.body;
+  if (token != process.env.ADMIN_TOKEN) {
+    log("[WEB] **ALERT**: Authentication failed for /update with invalid token: " + token);
+    res.status(401).end();
+    return;
+  }
+  updateFromWeb(updatemsg, updatebody);
+  res.status(202).end();
+})
