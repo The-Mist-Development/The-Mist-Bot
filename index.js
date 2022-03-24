6ed2c87855ee4137c6c0d1705cd6a8a896e7f3ea
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-03-24 21:02:52 +0000

    Website enabled by process.env.WEBSITE=TRUE

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
-// Run webserver
-const server = require("./server.js");
-
 // Run bot
 const bot = require("./botmain.js");
+
+
+// Run webserver
+if (process.env.WEBSITE == "TRUE") {
+    const server = require("./server.js"); 
+}
