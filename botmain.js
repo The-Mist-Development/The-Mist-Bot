023fa21534465675ee0d9a9a2516cddeb20225de
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-03-26 13:10:33 +0000

    update status

--- botmain.js
+++ botmain.js
@@ -18,7 +18,7 @@ client.login(token).catch(log);
 
 client.on("ready", async () => {
   ready = true;
-  client.user.setActivity(`${prefix}help | I am being refactored!`, { type: "LISTENING" });
+  client.user.setActivity(`${prefix}help | I'm v2!`, { type: "LISTENING" });
   log("[BOT] **Bot Started**");
   setClient(client);
   setup(client);
