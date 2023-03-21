238347a893a2ee8e1f86d04eff685aed7743164d
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2023-03-21 18:43:08 +0000

    Get multiple game prices from the api

--- wishlist_module/steamlib.js
+++ wishlist_module/steamlib.js
@@ -14,6 +14,24 @@ module.exports = {
                 });
         });
     },
+    getGamePrices: function (gameids) {
+        return new Promise(function (resolve, reject) {
+            axios.get(`https://store.steampowered.com/api/appdetails?appids=${gameids.join(",")}&cc=us&filters=price_overview`)
+                .then(function (response) {
+                    let formatted = {};
+                    for (let i = 0; i < gameids.length; i++) {
+                        if (response.data[gameids[i]].success) {
+                            formatted[gameids[i]] = response.data[gameids[i]].data;
+                        }
+                    }
+                    if (Object.keys(formatted).length > 0) resolve(formatted);
+                    else reject("GAMES_NOT_FOUND");
+                })
+                .catch(function (error) {
+                    reject(error.toString());
+                });
+        });
+    },
     getUserWishlist: function (snippet) {
         return new Promise(function (resolve, reject) {
             axios.get(`https://store.steampowered.com/wishlist${snippet}wishlistdata/`)
